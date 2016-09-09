4a7cfcdc6bcb6cdcea06896b6f438c3d2a9e9193
Suraj <suraj.aavula1993@gmail.com>
2016-09-09 13:56:34 +0530

    Update databasecon.java

--- databasecon.java
+++ databasecon.java
@@ -16,7 +16,7 @@ public class databasecon
         try
         {
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/robust", "root","root");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/suraj", "root","root");
         }
         catch(Exception e)
         {
