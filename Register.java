fc738a9c00991e717bc8467de3aea8bcc97b9a91
Suraj <suraj.aavula1993@gmail.com>
2016-09-10 16:02:31 +0530

    Update Register.java
    This servlet Register.java is used to allows a new user to create the account according to his role. Here the

--- Register.java
+++ Register.java
@@ -1,3 +1,4 @@
+/* This servlet Register.java is used to allow new user to create the account according to his role */
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -9,27 +10,29 @@ public class Register extends HttpServlet {
     
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
+        response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 	
         String name = request.getParameter("name");
         String email = request.getParameter("email");
         String pass = request.getParameter("pass");
-        try{
+        String role = request.getParameter("role");
+		try{
         
         //loading drivers for mysql
         Class.forName("com.mysql.jdbc.Driver");
 
 	//creating connection with the database 
           Connection  con=DriverManager.getConnection
-                     ("jdbc:mysql:/ /localhost:3306/test","username","password");
+                     ("jdbc:mysql:/ /localhost:3306/suraj","root","root");
 
         PreparedStatement ps=con.prepareStatement
-                  ("insert into Student values(?,?,?)");
+                  ("insert into NewUser values(?,?,?,?)");
 
         ps.setString(1, name);
         ps.setString(2, email);
         ps.setString(3, pass);
+	ps.setString(4, role);
         int i=ps.executeUpdate();
         
           if(i>0)
